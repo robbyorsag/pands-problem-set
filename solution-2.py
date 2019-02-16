c40f622e35dc667653fef397496058226ffb5a4e
Robert Orsag <00376369@gmit.ie>
2019-02-16 10:50:40 +0000

    Solution to problem 2

---
+++ solution-2.py
@@ -0,0 +1,17 @@
+" Solution to problem 2"
+
+import datetime
+# First we need to import date and time
+# so that Python knows how to calculate it
+
+if datetime.datetime.today().weekday() == 1:
+
+    
+    print ("Yes, today begins with a T")
+
+    if datetime.datetime.today().weekday() == 3:
+
+        print ("Yes, today begins with a T")
+
+else:
+     print (" No today does not begin with a T")
