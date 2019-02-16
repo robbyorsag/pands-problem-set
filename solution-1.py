67f89b2b4ed98e2564bfea2420412fa0d9c90185
Robert Orsag <00376369@gmit.ie>
2019-02-16 10:43:58 +0000

    Solution to problem 1

--- solution-1.py
+++ solution-1.py
@@ -1,1 +1,16 @@
 # Solution to problem 1
+
+x = int(input("Please enter a positive integer: "))
+# Entering a positive integer
+
+
+sum = 0
+# Setting the beggining value to zero
+
+for x in range(0,(x + 1)):
+
+  sum = sum + x
+
+
+print (sum)
+# prints the final value of sum
